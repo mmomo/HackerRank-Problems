9784ebfaccb1573725e152f6747144816faf808a
Cesar Venzor <cesarvenzor@protonmail.com>
2025-01-07 16:24:16 -0600

    solve Apple and Orange problem

--- HackerRankProblems.playground/Pages/01-AppleAndOrange.xcplaygroundpage/Contents.swift
+++ HackerRankProblems.playground/Pages/01-AppleAndOrange.xcplaygroundpage/Contents.swift
@@ -1,7 +1,23 @@
-//: [Previous](@previous)
-
 import Foundation
 
-var greeting = "Hello, playground"
+// https://www.hackerrank.com/challenges/apple-and-orange/problem?isFullScreen=true
 
-//: [Next](@next)
+func countApplesAndOranges(s: Int, t: Int, a: Int, b: Int, apples: [Int], oranges: [Int]) -> Void {
+    let houseRange = s...t
+    
+    let absoluteApplePositions = apples.map { $0 + a}
+    var absoluteOrangePositions = oranges.map { $0 + b}
+    
+    let appleCount = absoluteApplePositions.reduce(0) { count, apple in
+        count + (houseRange.contains(apple) ? 1 : 0)
+    }
+    
+    let orangeCount = absoluteOrangePositions.reduce(0) { count, orange in
+        count + (houseRange.contains(orange) ? 1 : 0)
+    }
+    
+    print(appleCount)
+    print(orangeCount)
+}
+
+countApplesAndOranges(s: 7, t: 11, a: 5, b: 15, apples: [-2,2,1], oranges: [5, -6])
